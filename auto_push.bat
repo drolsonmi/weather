a75ab8fe0b271fe556b31caa4e553c51ec87cdde
Michael Olson <michael.olson2@snow.edu>
2026-06-08 11:46:18 -0600

    Updated auto_push.bat

--- auto_push.bat
+++ auto_push.bat
@@ -2,9 +2,9 @@
 cd /d "C:\Users\GramSC\Documents\weather"
 
 :: Copy data files
-xcopy /E /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_Daily.dat" "C:\Users\GramSC\Documents\weather\data"
-xcopy /E /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FiveMin.dat" "C:\Users\GramSC\Documents\weather\data"
-xcopy /E /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FifteenMin.dat" "C:\Users\GramSC\Documents\weather\data"
+xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_Daily.dat" "C:\Users\GramSC\Documents\weather\data"
+xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FiveMin.dat" "C:\Users\GramSC\Documents\weather\data"
+xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FifteenMin.dat" "C:\Users\GramSC\Documents\weather\data"
 
 :: Stage all changes
 git add .
