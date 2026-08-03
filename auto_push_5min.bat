c3a0822c40031c48775398aa9feb5acdc5f9a72e
Michael Olson <michael.olson2@snow.edu>
2026-08-03 11:19:04 -0600

    Auto-update: Mon 08/03/2026 11:19:04.07

--- auto_push_5min.bat
+++ auto_push_5min.bat
@@ -4,6 +4,9 @@ cd /d "C:\Users\GramSC\Documents\weather"
 :: Update local repo
 git pull
 
+:: Clear garbage
+git gc --force
+
 :: Copy data files
 xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FiveMin.dat" "C:\Users\GramSC\Documents\weather\data"
 xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FifteenMin.dat" "C:\Users\GramSC\Documents\weather\data"
