005ca3302bf8cf3730cdd0c6888cbabe4a01561f
Michael Olson <michael.olson2@snow.edu>
2026-08-20 08:35:57 -0600

    Added 5-min and 15-min files

--- auto_push_daily.bat
+++ auto_push_daily.bat
@@ -8,10 +8,14 @@ git pull
 git gc --force
 
 :: Copy data files
+xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FiveMin.dat" "C:\Users\GramSC\Documents\weather\data"
+xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_FifteenMin.dat" "C:\Users\GramSC\Documents\weather\data"
 xcopy /I /Y "C:\Campbellsci\LoggerNet\Snow Weather_Daily.dat" "C:\Users\GramSC\Documents\weather\data"
 
 :: Stage all changes
-git add .
+git add "C:\Users\GramSC\Documents\weather\data\Snow Weather_FiveMin.dat"
+git add "C:\Users\GramSC\Documents\weather\data\Snow Weather_FifteenMin.dat"
+git add "C:\Users\GramSC\Documents\weather\data\Snow Weather_Daily.dat"
 
 :: Commit with a timestamp
 git commit -m "Auto-update: %DATE% %TIME%"
